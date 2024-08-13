5c5cf866c1a7e362ac331a7b087f582710cec0cd
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-08-13 19:18:48 -0400

    Add artist_name and notes_content methods

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,34 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  # def note_contents=()
+  #   self.notes.map(&:content)
+  # end
+
+  # def note_contents=(notes)
+  #   notes.each do |content|
+  #     note = Note.find(content: content)
+  #     self.notes << note
+  #   end
+  # end
+
+  def note_contents=(notes)
+    notes.each do |content|
+      note = Note.find_or_create_by(content: content)
+      self.notes << note
+    end
+  end
+
+  def note_contents
+    self.notes.map(&:content)
+  end
 end
